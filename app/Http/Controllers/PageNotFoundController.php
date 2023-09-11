7dfc582d95055f4a09a4bba7007fc249b630dd8e
Ghinan08 <alwaysongaming10@gmail.com>
2023-09-11 14:52:15 +0700

    memperbaiki modal my profile2

--- app/Http/Controllers/PageNotFoundController.php
+++ app/Http/Controllers/PageNotFoundController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use App\Http\Controllers\PageNotFoundController;
 
 class PageNotFoundController extends Controller
--- app/Http/Controllers/TjktController.php
+++ app/Http/Controllers/TjktController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use App\Http\Controllers\TjktController;
 
 class TjktController extends Controller
